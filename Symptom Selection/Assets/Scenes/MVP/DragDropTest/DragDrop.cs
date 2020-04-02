018b2053ea91715789ff1678efa04e0e190ae62e
Son Nguyen <sonnguyen4820@gmail.com>
2020-04-02 01:06:52 -0400

    MemoryGame

--- Symptom Selection/Assets/Scenes/MVP/DragDropTest/DragDrop.cs
+++ Symptom Selection/Assets/Scenes/MVP/DragDropTest/DragDrop.cs
@@ -5,7 +5,7 @@ using UnityEngine.EventSystems;
 
 public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
 {
-    [SerializeField] private Canvas canvas;
+    [SerializeField] private Canvas canvas; // fixed ratio for the dragged image to the canvas
 
     private RectTransform rectTransform;
     private CanvasGroup canvasGroup;
@@ -19,21 +19,21 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("OnBeginDrag");
-        canvasGroup.alpha = .6f;
-        canvasGroup.blocksRaycasts = false;
+        canvasGroup.alpha = .6f; // become transparent when dragged
+        canvasGroup.blocksRaycasts = false; // turn off the raycast
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         Debug.Log("OnDrag");
-        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor; // change the position of the dragged image depended on the canvas scale
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("OnEndDrag");
-        canvasGroup.alpha = 1f;
-        canvasGroup.blocksRaycasts = true;
+        canvasGroup.alpha = 1f; // return to normal when released
+        canvasGroup.blocksRaycasts = true; // turn on the raycast
     }
 
     public void OnPointerDown(PointerEventData eventData)
