75469f31b57758ae60a145bab3e74e8b13fe3323
Son Nguyen <sonnguyen4820@gmail.com>
2020-04-22 15:51:57 -0400

    Tutorial Upload

---
+++ Symptom Selection/Assets/Scenes/Build/Script/GMBegin.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GMBegin : MonoBehaviour
+{
+    public GameObject panel;
+
+    void Start()
+    {
+        panel.gameObject.SetActive(false);
+    }
+    public void Tutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("Play");
+    }
+    public void Credit()
+    {
+        panel.gameObject.SetActive(true);
+    }
+    public void Back()
+    {
+        panel.gameObject.SetActive(false);
+    }
+
+}
